af3f66deb857404495b4c13f9e9bd8604dc36ce1
Facu Stifman <fstifman@fi.uba.ar>
2026-03-19 20:59:25 -0300

    ejercicio ob greedy

--- TDA/greedy.py
+++ TDA/greedy.py
@@ -15,6 +15,19 @@ def charlas(horarios):
     return seleccionadas
 
 
+"""6-Se tiene un sistema monetario (ejemplo, el nuestro). Se quiere dar "cambio" de una determinada cantidad de plata. Implementar un algoritmo Greedy que devuelva el cambio pedido, usando la mínima cantidad de monedas/billetes. El algoritmo recibirá un arreglo de valores del sistema monetario, y la cantidad de cambio objetivo a dar, y debe devolver qué monedas/billetes deben ser utilizados para minimizar la cantidad total utilizada. Indicar y justificar la complejidad del algoritmo implementado. ¿El algoritmo implementado encuentra siempre la solución óptima? Justificar si es óptimo, o dar un contraejemplo. ¿Por qué se trata de un algoritmo Greedy? Justificar"""
+
+
+def cambio(monedas, monto):
+    monedas.sort(reverse=True)  # Ordenamos las monedas de mayor a menor valor
+    resultado = []
+    for moneda in monedas:
+        while monto >= moneda:
+            resultado.append(moneda)
+            monto -= moneda
+    return resultado
+
+
 """11- Las bolsas de un supermercado se cobran por separado y soportan hasta un peso máximo P, por encima del cual se rompen. Implementar un algoritmo greedy que, teniendo una lista de pesos de n productos comprados, encuentre la mejor forma de distribuir los productos en la menor cantidad posible de bolsas. Realizar el seguimiento del algoritmo propuesto para bolsas con peso máximo 5 y para una lista con los pesos: [ 4, 2, 1, 3, 5 ]. ¿El algoritmo implementado encuentra siempre la solución óptima? Justificar. Indicar y justificar la complejidad del algoritmo implementado.
 
 Nota sobre RPL: en este ejercicio se pide cumplir la tarea "con un algoritmo Greedy". Por las características de la herramienta, no podemos verificarlo de forma automática, pero se busca que se implemente con dicha restricción
@@ -25,19 +38,13 @@ Nota sobre RPL: en este ejercicio se pide cumplir la tarea "con un algoritmo Gre
 def bolsas(capacidad, productos):
     productos.sort(reverse=True)  # Ordenamos los productos de mayor a menor peso
     bolsas = []
-    pesos_actuales = []
-    for peso in productos:
-        if len(bolsas) == 0:
-            bolsas.append([peso])
-            pesos_actuales.append(peso)
-            continue
-        for i in range(len(bolsas)):
-            if pesos_actuales[i] + peso <= capacidad:
-                bolsas[i].append(peso)
-                pesos_actuales[i] += peso
+    for producto in productos:
+        colocado = False
+        for bolsa in bolsas:
+            if sum(bolsa) + producto <= capacidad:
+                bolsa.append(producto)
+                colocado = True
                 break
-            else:
-                bolsas.append([peso])
-                pesos_actuales.append(peso)
-
+        if not colocado:
+            bolsas.append([producto])  # Creamos una nueva bolsa para el producto
     return bolsas
